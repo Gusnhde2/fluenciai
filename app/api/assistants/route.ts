29cf40529164d4de6db289f01f8460c60a37d291
Kemal <lepuzanovickemal@gmail.com>
2023-11-27 18:46:44 +0100

    feat: create assistan api

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -8,12 +8,11 @@ export async function POST(req: NextRequest, res: NextResponse) {
   }
   const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+  const { name, lastname, language, sex } = await req.json();
+
   try {
     const myAssistant = await openai.beta.assistants.create({
-      instructions:
-        "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
-      name: "Math Tutor",
-      tools: [{ type: "code_interpreter" }],
+      instructions: `You are ${name} ${lastname}, a ${language}, ${sex} language learning assistant fluent in ${language} also friend of a user who wants to learn ${language}. Your goal is to help users learn a ${language} language. Greet the user, provide 3 conversation topics, be simple in answering, act like you are user's friend who is chating with him and sometimes gives advice on vocabulary, and grammar guidance. Encourage the user to ask questions and engage in interactive language exercises. Remember to reflect your ${language} background, ${sex} identity, and fluency in ${language}. Keep the conversation friendly and motivating.`,
       model: "gpt-4",
     });
     if (myAssistant) {
--- app/chat/@modal/page.tsx
+++ app/chat/@modal/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Button from "@/components/button/button";
 import Modal from "@/components/modal/modal";
 import { useChatContext } from "@/context/ChatContext";
+import { languages } from "./constatnts";
 
 export default function CreateAssistant() {
   const [loading, setLoading] = useState(false);
@@ -12,57 +13,86 @@ export default function CreateAssistant() {
   const context = useChatContext();
   const { state, dispatch } = context || {};
 
+  const nameRef = useRef<HTMLInputElement>(null);
+  const lastnameRef = useRef<HTMLInputElement>(null);
+  const languageRef = useRef<HTMLSelectElement>(null);
+  const sexRef = useRef<HTMLSelectElement>(null);
+
   async function createAssistant() {
     setLoading(true);
-    try {
-      const response = await fetch("/api/assistants", {
-        method: "POST",
-        // body: JSON.stringify({
-        //   name: "John Doe",
-        // }),
-      });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
+    const name = nameRef.current?.value;
+    const lastname = lastnameRef.current?.value;
+    const language = languageRef.current?.value;
+    const sex = sexRef.current?.value;
+    console.log(name, lastname, name?.trim().length);
+
+    if (name?.trim().length !== 0 && lastname?.trim().length !== 0) {
+      try {
+        const response = await fetch("/api/assistants", {
+          method: "POST",
+          body: JSON.stringify({
+            name: name,
+            lastname: lastname,
+            language: language,
+            sex: sex,
+          }),
+        });
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+
+          router.refresh();
+        }
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+        dispatch && dispatch({ type: "TOGGLE_MODAL" });
+      }
+    } else {
       setLoading(false);
-    } finally {
-      setLoading(false);
-      dispatch && dispatch({ type: "TOGGLE_MODAL" });
+      alert("Please enter a name and lastname");
     }
   }
 
   return (
     <Modal>
-      <h3>Create an assistant</h3>
-      <p>
-        You can create an assistant to help you with your daily tasks. You can
-        create as many assistants as you want.
-      </p>
-      <div>
-        <p>Give your assistant a name</p>
-        <input type="text" placeholder="Enter a name" />
-      </div>
-      <div>
-        <p>Select language</p>
-        <select>
-          <option value="en">English</option>
-          <option value="es">Spanish</option>
-          <option value="fr">French</option>
-        </select>
-      </div>
-      <div>
-        <p>Select sex</p>
-        <select>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-      </div>
-      <Button
-        text="Create assistant"
-        onClick={createAssistant}
-        loading={loading}
-      />
+      <form onSubmit={(event) => event?.preventDefault()}>
+        <h3>Create an assistant</h3>
+        <p>
+          You can create an assistant to help you with your daily tasks. You can
+          create as many assistants as you want.
+        </p>
+        <div>
+          <p>Give your assistant a name</p>
+          <input type="text" placeholder="Enter a name" ref={nameRef} />
+        </div>
+        <div>
+          <p>Give your assistant a lastname</p>
+          <input type="text" placeholder="Enter a lastname" ref={lastnameRef} />
+        </div>
+        <div>
+          <p>Select language</p>
+          <select ref={languageRef}>
+            {languages.map((language: any) => (
+              <option value={language}>{language}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p>Select sex</p>
+          <select ref={sexRef}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <Button
+          text="Create assistant"
+          onClick={createAssistant}
+          loading={loading}
+        />
+      </form>
     </Modal>
   );
 }
